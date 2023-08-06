8b13e26569ee44077876bd8baf7b14695dd2e963
Caio Gabriel <gabrielnunesdelima2003@gmail.com>
2023-08-05 23:49:46 -0300

    teste

--- Projeto_branch_Caio/src/Negocio/ControladorSistemaPadaria.java
+++ Projeto_branch_Caio/src/Negocio/ControladorSistemaPadaria.java
@@ -11,6 +11,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class ControladorSistemaPadaria {
 	Scanner scan=new Scanner(System.in);
@@ -35,8 +36,19 @@ public class ControladorSistemaPadaria {
 		return controlador;
 		
 	}
+	public void mostrarMenu() {
+		int opMostrarMenu=0;
+		System.out.println("Bem vindo ao sitema da mercearia \n1-Acesso ao menu\n-2Adicionar perfil");
+		try {
+			opMostrarMenu=scan.nextInt();
+			System.out.printf("Opção %i digitada...");
+		}catch(InputMismatchException e){
+			System.out.println("Entrada inválida o valor digitado não é um inteiro");
+		}
+	}
+	
 	public void adicionarFuncionario() {
-		System.out.println("Qual tipo de perfil deseja Adicionar\n1-Gerente\n2-Funcionário");
+		System.out.println("Qual tipo de funcionario deseja dicionar\n1-Gerente\n2-Funcionário");
 		String categoria="";
 		//OPÇÃO MENU
 		try{
@@ -55,6 +67,7 @@ public class ControladorSistemaPadaria {
 		System.out.println("Digite o nome do funcionario");
 		String nome=scan.nextLine();
 		
+		
 		//CPF DO FUNCIONARIO
 		System.out.println("Digite o cpf do funcionario em números");
 		String cpf=scan.nextLine();
@@ -63,29 +76,42 @@ public class ControladorSistemaPadaria {
 				cpfInt=Integer.parseInt(cpf);
 				System.out.printf("o numero do cpf é %i",cpfInt);
 		}
-		catch(Exception e){
+		catch(NumberFormatException  e){
 			System.out.println("Entrada inválida, o valor digitado não é um número");
 		}
+		
 		
 		//DATA DE NASCIMENTO
 		System.out.println("Digite a data de aniversario do funcionario no formato dd/MM/yyyy");
 		String dataAniversarioStr=scan.nextLine();
 		
 		DateTimeFormatter formatar=DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate dataAniversario=null; 
+		LocalDate dataAniversario=null;
+		
 		try {
-            
             dataAniversario = LocalDate.parse(dataAniversarioStr, formatar);
             
             System.out.println("Data de aniversário: " + dataAniversario.format(formatar));
-        } catch (Exception e) {
+        } catch (DateTimeParseException e) {
             System.out.println("Data inválida! Certifique-se de digitar a data no formato correto (dd/MM/yyyy).");
         }
 		
-		System.out.printf("Funcionario %s do tipo Gerente adicionado",nome);
-		Funcionario funcionario=new Funcionario(nome,cpfInt,dataAniversario,"",categoria);
-		repFuncionarios.cadastrarPerfil(funcionario);
 		
+		//ENDEREÇO DO FUNCIONARIO
+			System.out.println("Digite o cidade do funcionario:");
+			String cidade=scan.nextLine();	
+			
+			System.out.println("Digite o bairro:");
+			String bairro=scan.nextLine();
+			
+			System.out.println("Digite a rua e o número:");
+			String ruaNumero=scan.nextLine();
+		
+			String endereco=cidade+" "+bairro+" "+ruaNumero;
+		System.out.printf("Funcionario %s do tipo %s adicionado",nome,categoria);
+		
+		Funcionario funcionario=new Funcionario(nome,cpfInt,dataAniversario,endereco,categoria);
+		repFuncionarios.cadastrarPerfil(funcionario);
 		
 	}
 }
