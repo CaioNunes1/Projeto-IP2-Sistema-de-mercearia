c9551f07d2db5490bb6abef4fabfb4e3fe9b1e3a
Felipe Pereira <felipe.pereira.nave@gmail.com>
2023-08-07 21:30:32 -0300

    feat: Implementação do jfx (não concluída)

---
+++ Projeto_branch_Caio/src/Negocio/InterfaceGrafica.java
@@ -0,0 +1,22 @@
+package Negocio;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Hello JavaFX!");
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
--- Projeto_branch_Caio/src/Negocio/Main.java
+++ Projeto_branch_Caio/src/Negocio/Main.java
@@ -2,6 +2,6 @@ package Negocio;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
